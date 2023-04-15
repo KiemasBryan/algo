16f6d36f875b1adb900037839d5abb7bc722504a
root <root@DESKTOP-VMO4F5K.localdomain>
2023-04-15 10:13:56 +0700

    initial

---
+++ pert3/tipedata.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+//USING COMPILE DAN RUNNING
+// g++ namafile.cpp -o namefile &&./
+namafile
+
+int main(){
+    //TIPE DATA DASAR
+    int a = 10;
+    cout<<a<<endl;
+    return 0;
+}
